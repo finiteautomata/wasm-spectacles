2b0a2b5d09ba2941ff66a43ca395bdf6d820a01d
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-08-09 15:17:59 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-import * as wasm from "hello-wasm-pack";
 
-wasm.greet();
+import { TokenizerWasm } from "hf-tokenizers-wasm";
+
+class Tokenizer {
+  constructor(json) {
+    this.tokenizer = new TokenizerWasm(json);
+  }
+
+  static from_pretrained(name) {
+    return fetch(`https://huggingface.co/${name}/resolve/main/tokenizer.json`)
+      .then(response => response.text())
+      .then(json => new Tokenizer(json));
+  }
+
+  encode(text) {
+    return this.tokenizer.encode(text);
+  }
+}
+
+async function main() {
+    let tokenizer = await Tokenizer.from_pretrained("gpt2");
+    let encoding = tokenizer.encode("I love AI and privacy", false);
+    let contract = fs.readFileSync("contract.txt", "utf-8");
+    console.log(contract);
+    console.log(encoding.input_ids);
+    console.log(document);
+    document.tokenizer = tokenizer;
+}
+
+main();
